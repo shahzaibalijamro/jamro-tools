14d11f539d294aa9537f0713d0789fb76c852786
shahzaibalijamro <shahzaibalijamro@gmail.com>
2026-05-19 22:47:55 +0500

    setup google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <GoogleTagManager gtmId="GTM-K8T5VTBP" />
       <body className="min-h-full">{children}</body>
     </html>
   );
